b21129be51d07e7cbd4929561acf61a74a563701
David Adsit <david.adsit@gmail.com>
2012-05-24 21:46:01 -0600

    Failing Test 1

---
+++ hello-world/test/printer-test.js
@@ -0,0 +1,20 @@
+var buster = require('buster');
+var sinon = require('sinon');
+var printer = require('../lib/printer');
+
+buster.testCase("Printer", {
+    setUp: function() {
+        sinon.spy(console, "log");
+    },
+
+    tearDown: function() {
+        console.log.restore();
+    },
+
+    "calls console log with message": function() {
+        printer.printer.print("message");
+        assert.match(console.log.firstCall.args[0], /message/);
+    }
+
+});
+
